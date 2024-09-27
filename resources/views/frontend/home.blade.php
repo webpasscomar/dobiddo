228fcc93b0247395b16feb294560d6fe0662a6bf
Lean-2021 <leanw1711@gmail.com>
2024-09-26 22:29:12 -0300

    modificación hover en convocatorias mapa del home, correccion de zoom minimo y desplazamiento del mapa y eliminacion de codigo comentado no utilizado de la vista home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -9,8 +9,6 @@
             <div class="row">
 
                 <div class="col-md-6">
-                    <!-- <h2 class="fw-bold">¿Sabes cuántas convocatorias a consultorías se publican semanalmente?</h2> -->
-                    {{-- <br> </br> --}}
                     <p class="fs-4">
                         Como la mayoría de los consultores de hoy, seguramente te estás perdiendo de muchas
                         oportunidades.<br> </br>
@@ -26,21 +24,6 @@
                         Como sabemos que visitar todos los portales lleva mucho tiempo creamos dobiddo para que encuentres
                         todas las oportunidades en un solo lugar
                     </p>
-                    {{-- <p>No es sorprendente que hayas perdido una buena oportunidad ya que, tan solo entre los principales
-                        organismos internacionales,
-                        encontramos más de 20 plataformas de convocatorias que publican decenas de licitaciones por semana
-                        para
-                        países de Latinoamérica
-                        y el Caribe! </p>
-                    <p>
-                        Visitar todos los portales lleva mucho tiempo y es por eso que creamos dobiddo para que encuentres
-                        todas
-                        las oportunidades en un solo lugar.</p> --}}
-                    {{-- <p class="fs-4">
-          Dobiddo is a platform that provides specialized support services in identifying consulting opportunities in the
-          main international organizations.
-        </p> --}}
-                    {{-- <p class="fs-4 strong">We make it simple, digital, friendly & smart.</p> --}}
                 </div>
 
                 <div class="col-md-6">
@@ -52,167 +35,50 @@
                     <script>
                         var countriesWithCalls = @json($countriesWithCalls);
                         // Initialize the map
-                        var map = L.map('map').setView([-5, -80], 3);
+                        var map = L.map('map', {
+                            minZoom: 2 // Establecer nivel minimo de zoom en 2 para que no se vean zonas sin mapa
+                        }).setView([-5, -80], 3);
 
                         // Set up the OSM layer
                         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         }).addTo(map);
 
+                        // Establecer límites de desplazamiento para que no se vean zonas sin mapa
+                        let southWest = L.latLng(-90, -180);
+                        let northEast = L.latLng(90, 180);
+
+                        let bounds = L.latLngBounds(southWest, northEast);
+                        map.setMaxBounds(bounds);
+
+                        map.on('drag', function() {
+                            map.panInsideBounds(bounds, {
+                                animate: false
+                            });
+                        });
+
                         // Add the countries with calls to the map
                         countriesWithCalls.forEach(function(country) {
                             var marker = L.marker([country.lat, country.lon]).addTo(map);
+                            // Popup para mostrar convocatorias por país
                             marker.bindPopup(`
-            <b>${country.name}</b><br>
-            ${country.calls_count} convocatorias activas<br>
-            <a href="/calls/country/${country.id}">Ver convocatorias</a>
-        `);
+                                  <b>${country.name}</b><br>
+                                  ${country.calls_count} convocatorias activas<br>
+                                  <a href="/calls/country/${country.id}">Ver convocatorias</a>
+                              `);
+                            // Mostrar popup al hacer hover sobre el país
+                            marker.on('mouseover', function() {
+                                this.openPopup();
+                            });
+
+                            // cerrar popup luego de 4 segundos al dejar de hacer hover
+                            marker.on('mouseout', function() {
+                                setTimeout(() => {
+                                    this.closePopup();
+                                }, 4000);
+                            });
                         });
                     </script>
-                    {{-- <script>
-          var map = L.map('map').setView([20, 0], 2);
-
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-          }).addTo(map);
-
-          var convocatorias = [{
-              "pais": "Argentina",
-              "lat": -36.89536,
-              "lng": -60.31587
-            },
-            {
-              "pais": "Australia",
-              "lat": -25.2744,
-              "lng": 133.7751
-            },
-            {
-              "pais": "Brasil",
-              "lat": -14.2350,
-              "lng": -51.9253
-            },
-            {
-              "pais": "Canadá",
-              "lat": 56.1304,
-              "lng": -106.3468
-            },
-            {
-              "pais": "Chile",
-              "lat": -35.6751,
-              "lng": -71.5430
-            },
-            {
-              "pais": "China",
-              "lat": 35.8617,
-              "lng": 104.1954
-            },
-            {
-              "pais": "Colombia",
-              "lat": 4.5709,
-              "lng": -74.2973
-            },
-            {
-              "pais": "Egipto",
-              "lat": 26.8206,
-              "lng": 30.8025
-            },
-            {
-              "pais": "Francia",
-              "lat": 46.6034,
-              "lng": 1.8883
-            },
-            {
-              "pais": "Alemania",
-              "lat": 51.1657,
-              "lng": 10.4515
-            },
-            {
-              "pais": "India",
-              "lat": 20.5937,
-              "lng": 78.9629
-            },
-            {
-              "pais": "Indonesia",
-              "lat": -0.7893,
-              "lng": 113.9213
-            },
-            {
-              "pais": "Italia",
-              "lat": 41.8719,
-              "lng": 12.5674
-            },
-            {
-              "pais": "Japón",
-              "lat": 36.2048,
-              "lng": 138.2529
-            },
-            {
-              "pais": "México",
-              "lat": 23.6345,
-              "lng": -102.5528
-            },
-            {
-              "pais": "Nigeria",
-              "lat": 9.0820,
-              "lng": 8.6753
-            },
-            {
-              "pais": "Pakistán",
-              "lat": 30.3753,
-              "lng": 69.3451
-            },
-            {
-              "pais": "Perú",
-              "lat": -9.1899,
-              "lng": -75.0152
-            },
-            {
-              "pais": "Rusia",
-              "lat": 61.5240,
-              "lng": 105.3188
-            },
-            {
-              "pais": "Sudáfrica",
-              "lat": -30.5595,
-              "lng": 22.9375
-            },
-            {
-              "pais": "Corea del Sur",
-              "lat": 35.9078,
-              "lng": 127.7669
-            },
-            {
-              "pais": "España",
-              "lat": 40.4637,
-              "lng": -3.7492
-            },
-            {
-              "pais": "Turquía",
-              "lat": 38.9637,
-              "lng": 35.2433
-            },
-            {
-              "pais": "Reino Unido",
-              "lat": 55.3781,
-              "lng": -3.4360
-            },
-            {
-              "pais": "Estados Unidos",
-              "lat": 37.0902,
-              "lng": -95.7129
-            },
-            {
-              "pais": "Venezuela",
-              "lat": 6.4238,
-              "lng": -66.5897
-            }
-          ];
-
-          convocatorias.forEach(function(convocatoria) {
-            L.marker([convocatoria.lat, convocatoria.lng]).addTo(map)
-              .bindPopup('<b>' + convocatoria.pais + '</b>');
-          });
-        </script> --}}
                 </div>
 
             </div>
