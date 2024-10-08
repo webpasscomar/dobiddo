b963756a1bb7f0a4b938d0b8de6ea3fb0d1d6118
Eduardo Passarelli <epassarelli@gmail.com>
2024-10-08 11:41:51 -0300

    Se realizaron juntos los tickets 77 y 80

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,128 +2,128 @@
 @section('title', 'Inicio')
 
 @section('content')
-    {{-- Mapa y descripción del sitio --}}
-    <div class="container-fluid py-5 background_home_map">
-        <div class="container text-white">
+  {{-- Mapa y descripción del sitio --}}
+  <div class="container-fluid py-5 background_home_map">
+    <div class="container text-white">
 
-            <div class="row">
+      <div class="row">
 
-                <div class="col-md-6">
-                    <p class="fs-4">
-                        Como la mayoría de los consultores de hoy, seguramente te estás perdiendo de muchas
-                        oportunidades.<br> </br>
+        <div class="col-md-6">
+          <p class="fs-4">
+            Como la mayoría de los consultores de hoy, seguramente te estás perdiendo de muchas
+            oportunidades.<br> </br>
 
-                        Hay más de 20 plataformas de convocatorias en las que se actualizan semanalmente decenas de
-                        oportunidades. <br> </br>
-                        Solo los organismos internacionales publican más de 20 convocatorias de consultoría
-                        por semana solamente para América Latina y el Caribe.
-                        <br> </br>
-                        Somos un grupo de consultores apoya a la comunidad de consultores a encontrar las oportunidades de
-                        forma simple y fácil.
-                        <br> </br>
-                        Como sabemos que visitar todos los portales lleva mucho tiempo creamos dobiddo para que encuentres
-                        todas las oportunidades en un solo lugar
-                    </p>
-                </div>
+            Hay más de 20 plataformas de convocatorias en las que se actualizan semanalmente decenas de
+            oportunidades. <br> </br>
+            Solo los organismos internacionales publican más de 20 convocatorias de consultoría
+            por semana solamente para América Latina y el Caribe.
+            <br> </br>
+            Somos un grupo de profesionales que apoya a la comunidad de consultores a encontrar las oportunidades de
+            forma simple y fácil.
+            <br> </br>
+            Como sabemos que visitar todos los portales lleva mucho tiempo creamos dobiddo para que encuentres
+            todas las oportunidades en un solo lugar
+          </p>
+        </div>
 
-                <div class="col-md-6">
-                    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
-                    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+        <div class="col-md-6">
+          <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+          <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
 
-                    <div id="map" style="width: 100%; height: 650px;" class="rounded-3"></div>
+          <div id="map" style="width: 100%; height: 650px;" class="rounded-3"></div>
 
-                    <script>
-                        var countriesWithCalls = @json($countriesWithCalls);
-                        // Initialize the map
-                        var map = L.map('map', {
-                            minZoom: 2 // Establecer nivel minimo de zoom en 2 para que no se vean zonas sin mapa
-                        }).setView([-5, -80], 3);
+          <script>
+            var countriesWithCalls = @json($countriesWithCalls);
+            // Initialize the map
+            var map = L.map('map', {
+              minZoom: 2 // Establecer nivel minimo de zoom en 2 para que no se vean zonas sin mapa
+            }).setView([-5, -80], 3);
 
-                        // Set up the OSM layer
-                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        }).addTo(map);
+            // Set up the OSM layer
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
-                        // Establecer límites de desplazamiento para que no se vean zonas sin mapa
-                        let southWest = L.latLng(-90, -180);
-                        let northEast = L.latLng(90, 180);
+            // Establecer límites de desplazamiento para que no se vean zonas sin mapa
+            let southWest = L.latLng(-90, -180);
+            let northEast = L.latLng(90, 180);
 
-                        let bounds = L.latLngBounds(southWest, northEast);
-                        map.setMaxBounds(bounds);
+            let bounds = L.latLngBounds(southWest, northEast);
+            map.setMaxBounds(bounds);
 
-                        map.on('drag', function() {
-                            map.panInsideBounds(bounds, {
-                                animate: false
-                            });
-                        });
+            map.on('drag', function() {
+              map.panInsideBounds(bounds, {
+                animate: false
+              });
+            });
 
-                        // Add the countries with calls to the map
-                        countriesWithCalls.forEach(function(country) {
-                            var marker = L.marker([country.lat, country.lon]).addTo(map);
-                            // Popup para mostrar convocatorias por país
-                            marker.bindPopup(`
+            // Add the countries with calls to the map
+            countriesWithCalls.forEach(function(country) {
+              var marker = L.marker([country.lat, country.lon]).addTo(map);
+              // Popup para mostrar convocatorias por país
+              marker.bindPopup(`
                                   <b>${country.name}</b><br>
                                   ${country.calls_count} convocatorias activas<br>
                                   <a href="/calls/country/${country.id}">Ver convocatorias</a>
                               `);
-                            // Mostrar popup al hacer hover sobre el país
-                            marker.on('mouseover', function() {
-                                this.openPopup();
-                            });
+              // Mostrar popup al hacer hover sobre el país
+              marker.on('mouseover', function() {
+                this.openPopup();
+              });
 
-                            // cerrar popup luego de 4 segundos al dejar de hacer hover
-                            marker.on('mouseout', function() {
-                                setTimeout(() => {
-                                    this.closePopup();
-                                }, 4000);
-                            });
-                        });
-                    </script>
-                </div>
-
-            </div>
-
+              // cerrar popup luego de 4 segundos al dejar de hacer hover
+              marker.on('mouseout', function() {
+                setTimeout(() => {
+                  this.closePopup();
+                }, 4000);
+              });
+            });
+          </script>
         </div>
+
+      </div>
+
     </div>
+  </div>
 
-    {{-- enlaces Organismos y Consultores --}}
-    <div class="container">
+  {{-- enlaces Organismos y Consultores --}}
+  <div class="container">
 
-        <div class="row">
+    <div class="row">
 
-            <div class="col-lg-6 text-center my-4">
-                <div class="p-4 m-4 shadow bg-light rounded-3">
-                    <h2>¿Eres un Organismo?</h2>
-                    <p class="lead fs-6">Contacta con nosotros si deseas publicar convocatorias.</p>
-                    <a href="{{ route('institutions') }}" class="btn text-white btn-lg btn_institutions-submit">Publicar
-                        Convocatorias</a>
-                </div>
-            </div>
-
-            <div class="col-lg-6 text-center my-4">
-                <div class="p-4 m-4 shadow bg-light rounded-3">
-                    <h2>¿Eres un Consultor?</h2>
-                    <p class="lead fs-6">Sumate a la Comunidad</p>
-                    <a href="{{ route('consultans') }}" class="btn btn-lg btn_consultants-submit">Recibir
-                        Convocatorias</a>
-                </div>
-            </div>
-
+      <div class="col-lg-6 text-center my-4">
+        <div class="p-4 m-4 shadow bg-light rounded-3">
+          <h2>¿Eres un Organismo?</h2>
+          <p class="lead fs-6">Contacta con nosotros si deseas publicar convocatorias.</p>
+          <a href="{{ route('institutions') }}" class="btn text-white btn-lg btn_institutions-submit">Publicar
+            Convocatorias</a>
         </div>
-    </div>
+      </div>
 
-    {{-- Imágenes --}}
-
-    <div class="container-fluid home_images">
-        <div class="row">
-            <div class="col-md-6 d-flex align-items-center justify-content-center">
-                <img src="{{ asset('img/mensajes_frases/Mensaje1.png') }}" class="img-fluid" alt="Doing bidding simple"
-                    width="700">
-            </div>
-
-            <div class="col-md-6 d-flex align-items-center justify-content-center">
-                <img src="{{ asset('img/home/home.png') }}" class="img-fluid" alt="Doing bidding simple">
-            </div>
+      <div class="col-lg-6 text-center my-4">
+        <div class="p-4 m-4 shadow bg-light rounded-3">
+          <h2>¿Eres un Consultor?</h2>
+          <p class="lead fs-6">Sumate a la Comunidad</p>
+          <a href="{{ route('consultans') }}" class="btn btn-lg btn_consultants-submit">Recibir
+            Convocatorias</a>
         </div>
+      </div>
+
     </div>
+  </div>
+
+  {{-- Imágenes --}}
+
+  <div class="container-fluid home_images">
+    <div class="row">
+      <div class="col-md-6 d-flex align-items-center justify-content-center">
+        <img src="{{ asset('img/mensajes_frases/Mensaje1.png') }}" class="img-fluid" alt="Doing bidding simple"
+          width="700">
+      </div>
+
+      <div class="col-md-6 d-flex align-items-center justify-content-center">
+        <img src="{{ asset('img/home/home.png') }}" class="img-fluid" alt="Doing bidding simple">
+      </div>
+    </div>
+  </div>
 @endsection
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,200 +1,206 @@
-    <!-- footer -->
+<!-- footer -->
 
-    {{-- mostrar logo dobiddo solo en las rutas de los formularios publicar organismos y convocatorias --}}
-    @if (request()->routeIS('consultans') || request()->routeIS('institutions'))
-      <div
-        class="container-fluid text-center {{ request()->routeIS('institutions') ? 'section_logo_dobiddo-institutions' : 'section_logo_dobiddo-consultants' }} section_logo_dobiddo mt-5 mt-xl-0">
-        <div class="row">
-          <div class="col m-auto">
-            <img src="{{ asset('img/logotipos/10 Logo claim centrado negro.png') }}" class="" width="200"
-              alt="dobiddo the bid doers">
-          </div>
-        </div>
-      </div>
-    @endif
+<div class="footer" style="background-color: #333">
 
-    {{-- Mostrar en convocatorias y detalles de las convocatorias --}}
-    @if (request()->routeIs('calls') || request()->routeIS('calls.detail') || request()->routeIS('calls.country'))
-      <div class="container-fluid calls_banner py-4 mt-5">
-        <div class="container">
-          <div class="row gy-4 gy-md-0 align-items-center text-center">
-            <div class="col-md-4">
-              <img src="{{ asset('img/mensajes_frases/Mensaje2-mod.png') }}" alt="We do. You go."
-                class="img-fluid calls_banner-images">
-            </div>
-            <div class="col-md-4">
-              <img src="{{ asset('img/elementos/reloj-mod.png') }}" alt="reloj"
-                class="imf-fluid calls_banner-images">
-            </div>
-            <div class="col-md-4">
-              <img src="{{ asset('img/logotipos/12 Logo claim centrado blanco.png') }}" alt="dobiddo the bid doers"
-                class="img-fluid calls_banner-images">
-            </div>
-          </div>
-        </div>
-      </div>
-    @endif
-    {{-- Footer --}}
-    <div class="footer" style="background-color: #333">
-      <div class="container py-4">
-        <div class="row">
-          <!-- contacto -->
-          <div class="col-md-5 mb-3">
-            <p class="h6 fw-bold text-white">Contacto</p>
+  <div class="container py-4">
 
-            <p class="text-white"><a href="mailto: info@dobiddo.com" class="link_webpass" target="_blank">Email:
-                info@dobiddo.com</a>
-            </p>
+    <div class="row">
 
-          </div>
+      <div class="col-md-2">
+        <p class="h6 fw-bold text-white">AFIP</p>
 
-          <!-- categorias -->
-          <div class="col-md-5 mb-3">
-            <p class="h6 fw-bold text-white">Otros</p>
-            <ul class="list-unstyled text-white-50 light">
-              <li>
-                <a href="{{ route('policies') }}" class="text-decoration-none link-light"
-                  title="Politicas de privacidad">Politicas de privacidad
-                </a>
-              </li>
-              <li>
-                <a href="https://webpass.com.ar" class="link_webpass" target="_blank">Diseño y Desarrollo</a> by WebPass
-              </li>
-            </ul>
-          </div>
-
-
-          <!-- redes -->
-          <div class="col-md-2">
-            <p class="h6 fw-bold text-white">Nuestras redes</p>
-            <ul class="list-group list-group-horizontal">
-              <li class="list-group-item bg-transparent ps-0 border-0 light text-light">
-                <a href="mailto:info@dobiddo.com" class="text-decoration-none link-light" title="Envianos un email"> <i
-                    class="fa-solid fa-envelope fs-5"></i> </a>
-              </li>
-              <li class="list-group-item bg-transparent ps-0 border-0 light text-light">
-                <a href="https://www.instagram.com/thebiddoers/" target="_blank" class="text-decoration-none link-light"
-                  title="Nuestro Instagram"> <i class="fa-brands fa-instagram fs-5"></i> </a>
-              </li>
-            </ul>
-          </div>
-
-        </div>
+        <p class="text-white">
+          <a href="http://qr.afip.gob.ar/?qr=xf92ZTjtbvkQiUcQdW5vdQ,," target="_F960AFIPInfo"><img
+              src="{{ asset('img/qr.jpg') }}" border="0" width="90px"></a>
+        </p>
 
       </div>
+
+
+      <!-- contacto -->
+      <div class="col-md-4">
+        <p class="h6 fw-bold text-white">Menú principal</p>
+        <ul class="text-white">
+          <li><a href="" class="link_webpass" target="_blank">Inicio</a></li>
+          <li><a href="" class="link_webpass" target="_blank">Nosotros</a></li>
+          <li><a href="" class="link_webpass" target="_blank">Que hacemos</a></li>
+          <li><a href="" class="link_webpass" target="_blank">Convocatorias</a></li>
+        </ul>
+      </div>
+
+      <!-- categorias -->
+      <div class="col-md-4">
+        <p class="h6 fw-bold text-white">Otros</p>
+        <ul class="list-unstyled text-white-50 light">
+          <li>
+            <a href="#" class="text-decoration-none link-light" title="Politicas de privacidad">Politicas de
+              privacidad
+            </a>
+            {{--  {{ route('policies') }} --}}
+          </li>
+
+        </ul>
+        <p class="h6 fw-bold text-white">Contacto</p>
+        <a href="mailto:info@dobiddo.com" class="text-decoration-none link-light" title="Envianos un email">
+          info@dobiddo.com </a>
+      </div>
+
+
+      <!-- redes -->
+      <div class="col-md-2">
+        <p class="h6 fw-bold text-white">Nuestras redes</p>
+        <ul class="list-group list-group-horizontal">
+          <li class="list-group-item bg-transparent ps-0 border-0 light text-light">
+            <a href="mailto:info@dobiddo.com" class="text-decoration-none link-light" title="Envianos un email"> <i
+                class="fa-solid fa-envelope fs-5"></i> </a>
+          </li>
+          <li class="list-group-item bg-transparent ps-0 border-0 light text-light">
+            <a href="https://www.instagram.com/thebiddoers/" target="_blank" class="text-decoration-none link-light"
+              title="Nuestro Instagram"> <i class="fa-brands fa-instagram fs-5"></i> </a>
+          </li>
+          <li class="list-group-item bg-transparent ps-0 border-0 light text-light">
+            <a href="https://www.linkedin.com/company/dobiddo" target="_blank" class="text-decoration-none link-light"
+              title="Nuestro LinkedIn"> <i class="fa-brands fa-linkedin fs-5"></i> </a>
+          </li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
+<div class="copyrights" style="background-color: #222">
+  <div class="container">
+    <div class="row pt-4">
+
+      <div class="col-md-6">
+        <p class="text-white">Copyrights &copy; {{ date('Y') }} - Todos los derechos reservados.</p>
+      </div>
+
+      <div class="col-md-6">
+        <p class="text-white text-end"><a href="https://webpass.com.ar" class="link_webpass h6 fw-bold "
+            target="_blank">Diseño
+            y
+            Desarrollo</a> by WebPass</p>
+      </div>
+
     </div>
 
-    @stack('js')
-    <!-- FIN footer -->
+  </div>
+</div>
+
+@stack('js')
+<!-- FIN footer -->
 
 
-    <script type="text/javascript" charset="utf-8">
-      // tooltip
-      var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-      var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-      });
+<script type="text/javascript" charset="utf-8">
+  // tooltip
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
 
-      // menú dropdown hover
-      const $dropdown = $(".dropdown");
-      const $dropdownToggle = $(".dropdown-toggle");
-      const $dropdownMenu = $(".dropdown-menu");
-      const showClass = "show";
+  // menú dropdown hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
 
-      $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 768px)").matches) {
-          $dropdown.hover(
-            function() {
-              const $this = $(this);
-              $this.addClass(showClass);
-              $this.find($dropdownToggle).attr("aria-expanded", "true");
-              $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-              const $this = $(this);
-              $this.removeClass(showClass);
-              $this.find($dropdownToggle).attr("aria-expanded", "false");
-              $this.find($dropdownMenu).removeClass(showClass);
-            }
-          );
-        } else {
-          $dropdown.off("mouseenter mouseleave");
+  $(window).on("load resize", function() {
+    if (this.matchMedia("(min-width: 768px)").matches) {
+      $dropdown.hover(
+        function() {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function() {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
         }
-      });
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
 
-      // search animado
-      const searchBtn = document.querySelector('#searchBtn');
-      const animatedInput = document.querySelector('#animated-input');
+  // search animado
+  const searchBtn = document.querySelector('#searchBtn');
+  const animatedInput = document.querySelector('#animated-input');
 
-      searchBtn.addEventListener('click', openSearch);
+  searchBtn.addEventListener('click', openSearch);
 
-      function openSearch(e) {
-        animatedInput.focus();
-      }
-      // Check if there is text in input every 50ms
-      setInterval(function() {
-        if (animatedInput.value) {
-          animatedInput.style.width = '225px';
-        }
-      }, 50);
-    </script>
+  function openSearch(e) {
+    animatedInput.focus();
+  }
+  // Check if there is text in input every 50ms
+  setInterval(function() {
+    if (animatedInput.value) {
+      animatedInput.style.width = '225px';
+    }
+  }, 50);
+</script>
 
 
-    <script>
-      function shareOnWhatsApp(url) {
-        window.open(`https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`, '_blank');
-      }
+<script>
+  function shareOnWhatsApp(url) {
+    window.open(`https://api.whatsapp.com/send?text=${encodeURIComponent(url)}`, '_blank');
+  }
 
-      function shareByEmail(url) {
-        window.location.href =
-          `mailto:?subject=Interesante convocatoria&body=Echa un vistazo a esta convocatoria: ${encodeURIComponent(url)}`;
-      }
+  function shareByEmail(url) {
+    window.location.href =
+      `mailto:?subject=Interesante convocatoria&body=Echa un vistazo a esta convocatoria: ${encodeURIComponent(url)}`;
+  }
 
-      function shareOnTwitter(url) {
-        window.open(
-          `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=Echa un vistazo a esta convocatoria`,
-          '_blank');
-      }
+  function shareOnTwitter(url) {
+    window.open(
+      `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}&text=Echa un vistazo a esta convocatoria`,
+      '_blank');
+  }
 
-      function shareOnFacebook(url) {
-        window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank');
-      }
+  function shareOnFacebook(url) {
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`, '_blank');
+  }
 
-      function copyToClipboard(url) {
-        navigator.clipboard.writeText(url).then(() => {
-          alert('URL copiada al portapapeles');
-        }, () => {
-          alert('Error al copiar la URL');
-        });
-      }
-    </script>
+  function copyToClipboard(url) {
+    navigator.clipboard.writeText(url).then(() => {
+      alert('URL copiada al portapapeles');
+    }, () => {
+      alert('Error al copiar la URL');
+    });
+  }
+</script>
 
-    <script>
-      function addToCalendar(title, organization, expiration) {
-        const event = {
-          title: title,
-          start: expiration,
-          description: `Organizado por ${organization}`,
-          url: window.location.href
-        };
+<script>
+  function addToCalendar(title, organization, expiration) {
+    const event = {
+      title: title,
+      start: expiration,
+      description: `Organizado por ${organization}`,
+      url: window.location.href
+    };
 
-        const eventString = encodeURIComponent(
-          `BEGIN:VCALENDAR\n` +
-          `VERSION:2.0\n` +
-          `BEGIN:VEVENT\n` +
-          `SUMMARY:${event.title}\n` +
-          `DESCRIPTION:${event.description}\n` +
-          `DTSTART:${event.start.replace(/-/g, '')}T000000Z\n` +
-          `URL:${event.url}\n` +
-          `END:VEVENT\n` +
-          `END:VCALENDAR`
-        );
+    const eventString = encodeURIComponent(
+      `BEGIN:VCALENDAR\n` +
+      `VERSION:2.0\n` +
+      `BEGIN:VEVENT\n` +
+      `SUMMARY:${event.title}\n` +
+      `DESCRIPTION:${event.description}\n` +
+      `DTSTART:${event.start.replace(/-/g, '')}T000000Z\n` +
+      `URL:${event.url}\n` +
+      `END:VEVENT\n` +
+      `END:VCALENDAR`
+    );
 
-        const downloadLink = document.createElement('a');
-        downloadLink.href = `data:text/calendar;charset=utf-8,${eventString}`;
-        downloadLink.download = `${event.title}.ics`;
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      }
-    </script>
+    const downloadLink = document.createElement('a');
+    downloadLink.href = `data:text/calendar;charset=utf-8,${eventString}`;
+    downloadLink.download = `${event.title}.ics`;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
+</script>
